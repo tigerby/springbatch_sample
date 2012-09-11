dbe71ab5bb68d25c40bbe0fc8ee712aa0211332a
unknown <bongyeonkim@gmail.com>
2012-09-11 00:05:08 -0700

    init project

---
+++ src/main/java/com/apress/springbatch/statement/domain/Weather.java
@@ -0,0 +1,11 @@
+package com.apress.springbatch.statement.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tiger
+ * Date: 9/10/12
+ * Time: 11:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Weather {
+}
